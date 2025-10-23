fe7a0d62d37445ebd4f72d62b9729eecc6261d93
Yannou7 <yann.melisse.jf@outlook.fr>
2025-10-23 22:54:25 +0200

    eat glass and die

--- Jeu-societe/AProject/Graphics/TextPlus.hpp
+++ Jeu-societe/AProject/Graphics/TextPlus.hpp
@@ -11,14 +11,19 @@
 class TextPlus : public sf::Text
 {
 	private:
+		// Unedited string
 		std::string m_string;
+
+		// 0 : String Start
+		// 1 : String End
 		int m_textRange[2];
 
-		// Origin in percentages
+		// Origin in coefficients
 		sf::Vector2f m_origin;
 
 	public:
 		TextPlus(void);
+
 
 		void setString(const std::string& _string);
 };
