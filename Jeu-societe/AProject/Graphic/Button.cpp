4562c48503600a0fafe252b1b3e86d37b686abeb
kUwUllion <kyllian.nb@outlook.fr>
2025-10-31 10:35:24 +0100

    ajt fonctionnalités boutons
    non fini + a tester

--- Jeu-societe/AProject/Graphic/Button.cpp
+++ Jeu-societe/AProject/Graphic/Button.cpp
@@ -2,15 +2,87 @@
 
 Button::Button(void):SpriteAnimated()
 {
+
+}
+
+void Button::Update(float _dt)
+{
 	
 }
 
+void Button::Draw(sf::RenderTarget& target, sf::RenderStates states)
+{
+	SpriteAnimated::draw(target, states);
+}
+
+void Button::UpdateTexture()
+{
+	sf::Vector2f mousePos = (sf::Vector2f)sf::Mouse::getPosition();
+	
+	if (!isMouseOn(mousePos) && !isClicked(mousePos))
+	{
+		SpriteAnimated::SetAnimation("IDLE", true);
+	}
+	else if (isMouseOn(mousePos) && !isClicked(mousePos))
+	{
+		SpriteAnimated::SetAnimation("ON", true);
+	}
+	else if (isClicked(mousePos))
+	{
+		SpriteAnimated::SetAnimation("HELD", true);
+	}
+
+}
+
+
+void Button::SetTexure(TextureAnimated _texture)
+{
+	SpriteAnimated::SetTexture(_texture);
+}
+
+void Button::SetPosition(sf::Vector2f _pos)
+{
+	SpriteAnimated::setPosition(_pos);
+}
+
+void Button::SetOrigin(sf::Vector2f _origin)
+{
+	this->m_originPercent = _origin;
+}
+
+
 bool Button::isMouseOn(sf::Vector2f _mousePos)
 {
-	sf::FloatRect bounds = SpriteAnimated::getGlobalBounds();
-	if (bounds.contains(_mousePos))
+	sf::FloatRect rect = Sprite::getGlobalBounds();
+	if (rect.contains(_mousePos))
 	{
 		return true;
 	}
 	return false;
 }
+
+bool Button::isClicked(sf::Vector2f _mousePos)
+{
+	if (this->isMouseOn(_mousePos) && (sf::Mouse::isButtonPressed(sf::Mouse::Left)))
+	{
+		return true;
+	}
+	return false;
+}
+
+
+SpriteAnimated Button::GetSpriteAnimated()
+{
+	return SpriteAnimated();
+}
+
+sf::Sprite Button::getSprite()
+{
+	return SpriteAnimated::Sprite();
+}
+
+sf::Vector2f Button::getPosition()
+{
+	return SpriteAnimated::getPosition();
+}
+
--- Jeu-societe/AProject/Graphic/Button.hpp
+++ Jeu-societe/AProject/Graphic/Button.hpp
@@ -10,8 +10,31 @@ class Button : public SpriteAnimated
 	
 		Button(void);
 
+		void Update(float _dt);
+		void Draw(sf::RenderTarget& target, sf::RenderStates states);
+
+		void UpdateTexture();
+
+		void SetTexure(TextureAnimated _texture);
+		void SetPosition(sf::Vector2f _pos);
+
+
+		void SetOrigin(sf::Vector2f _origin);
+
 		bool isMouseOn(sf::Vector2f _mousePos);
+		//bool isSelected();
+		bool isClicked(sf::Vector2f _mousePos);
+
+
+		SpriteAnimated GetSpriteAnimated();
+		sf::Sprite getSprite();
+
+		sf::Vector2f getPosition();
+
+
+
 	private: 
+		sf::Vector2f m_originPercent;
 };
 	
 #endif // !_INC_BUTTON_HPP
