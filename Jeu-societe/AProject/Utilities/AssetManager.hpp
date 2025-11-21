d5bb99df55b309734e0acf4631b1288b94b7dad3
Caramanis Pierre <pc30320@outlook.fr>
2025-11-21 11:21:57 +0100

    e

--- Jeu-societe/AProject/Utilities/AssetManager.hpp
+++ Jeu-societe/AProject/Utilities/AssetManager.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #ifndef _INC_ASSETMANAGER_HPP
 #define _INC_ASSETMANAGER_HPP
 
@@ -128,7 +127,7 @@ class AssetManager
 
 
 		// Get any asset associated with this name from last to first container
-		void* GetAsset(const std::string& _name, AssetType _type);
+		void* GetAsset(const std::string& _name, AssetType _type = AssetType::UNKNOWN);
 
 		// Get an asset associated with this name from last to first container,
 		// If the asset is not found, tries to get the default placeholder asset
@@ -141,7 +140,7 @@ class AssetManager
 		// Get an asset associated with this name from last to first container,
 		// If the asset is not found, gets the default placeholder available for this type
 		template <typename T>
-		T* GetAsset(const std::string& _name, AssetType _type);
+		T* GetAsset(const std::string& _name, AssetType _type = AssetType::UNKNOWN);
 
 	private:
 		std::vector<Container> m_containers;
@@ -176,7 +175,11 @@ inline void AssetManager::AddAsset(const std::string& _name, T* _object, AssetTy
 	this->GetContainer(_containerID).AddAsset<T>(_name, _object, _type);
 }
 
-
+template<typename T>
+inline T* AssetManager::GetAsset(const std::string& _name, AssetType _type)
+{
+	return (T*)this->GetAssetOrPlaceholder(_name, _type);
+}
 
 #endif
-// Asset Manager C++ v1.0.2
+// Asset Manager C++ v1.0.5
