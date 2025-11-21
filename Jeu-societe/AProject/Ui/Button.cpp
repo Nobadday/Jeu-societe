b8e0d60e43858d4d8859452ad61e368a1f09bd4d
Yannou7 <yann.melisse.jf@outlook.fr>
2025-11-21 17:04:03 +0100

    sewerslide

--- Jeu-societe/AProject/Ui/Button.cpp
+++ Jeu-societe/AProject/Ui/Button.cpp
@@ -1,94 +1,36 @@
 #include "Button.hpp"
 
-Button::Button():SpriteAnimated()
+Button::Button(void) : SpriteAnimated(),
+m_currentState	(STATE_IDLE),
+m_isClicked		(false)
 {
-	this->m_currentState = STATE_IDLE;
-	this->m_isClicked = false;
+
 }
 
-
-
-void Button::OnFrameChanged(void)
+bool Button::CheckEvent(const sf::Event& _event)
 {
-	this->SpriteAnimated::OnFrameChange();
-}
-
-void Button::PollEvent(sf::Event& _event)
-{
-
-
-	if (!this->m_isClicked)
+	switch (_event.type)
 	{
-		switch (_event.type)
-		{
 		case sf::Event::MouseButtonPressed:
-			if (this->GetState() == STATE_ON)
-			{
-				if (this->IsClicked(_event.mouseButton.x, _event.mouseButton.y))
-				{
-					this->SetState(STATE_PRESSED);
-				}
-			}
-			break;
-
 		case sf::Event::MouseButtonReleased:
-			if (this->GetState() == STATE_PRESSED)
+			if (_event.mouseButton.button == sf::Mouse::Left)
 			{
-				if (_event.mouseButton.button == sf::Mouse::Left && this->IsMouseOn(_event.mouseButton.x, _event.mouseButton.y))
+				if (this->CheckMouseCollision(_event.mouseButton.x, _event.mouseButton.y))
 				{
-					this->m_isClicked = true;
-				}
-				else
-				{
-					this->SetState(STATE_IDLE);
+					return this->CheckMouseClick(_event.type == sf::Event::MouseButtonReleased);
 				}
 			}
 			break;
 
 		case sf::Event::MouseMoved:
-
-			switch (this->GetState())
-			{
-				case STATE_IDLE:
-					if (this->IsMouseOn(_event.mouseMove.x, _event.mouseMove.y))
-					{
-						this->SetState(STATE_ON);
-					}
-					break;
-				case STATE_ON:
-					if (!this->IsMouseOn(_event.mouseMove.x, _event.mouseMove.y))
-					{
-						this->SetState(STATE_IDLE);
-					}
-					break;
-				default:
-					break;
-			}
+			return this->CheckMouseCollision(_event.mouseMove.x, _event.mouseMove.y);
 			break;
 
 		default:
 			break;
-		}
-	}	
-}
 
-bool Button::IsMouseOn(const sf::Vector2f& _mousePos)
-{
-	return this->getGlobalBounds().contains(_mousePos);
-}
-bool Button::IsMouseOn(int _mouseX, int _mouseY)
-{
-	return this->getGlobalBounds().contains((float)_mouseX, (float)_mouseY);
-}
-
-
-bool Button::IsClicked(const sf::Vector2f& _mousePos)
-{
-	return (this->IsMouseOn(_mousePos) && sf::Mouse::isButtonPressed(sf::Mouse::Left));
-}
-bool Button::IsClicked(int _mouseX, int _mouseY)
-{
-	return (this->IsMouseOn(_mouseX, _mouseY) && sf::Mouse::isButtonPressed(sf::Mouse::Left));
+	}
+	return false;
 }
 
 
@@ -104,23 +46,61 @@ bool Button::HasBeenClicked(void)
 }
 
 
+bool Button::CheckMouseCollision(const sf::Vector2i& _mousePos)
+{
+	return this->CheckMouseCollision(_mousePos.x, _mousePos.y);
+}
+bool Button::CheckMouseCollision(int _x, int _y)
+{
+	return this->getGlobalBounds().contains(_x, _y);
+}
+
+bool Button::CheckMouseClick(bool _isReleased)
+{
+	if (!this->m_isClicked)
+	{
+		if (_isReleased)
+		{
+			// Confirmation click
+			if (this->m_currentState == STATE_PRESSED)
+			{
+				this->m_isClicked = true;
+				this->SetState(STATE_IDLE);
+				return true;
+			}
+			// Else : Missclick
+		}
+		else
+		{
+			// Initiation click
+			if ((this->m_currentState == STATE_IDLE) || (this->m_currentState == STATE_ON))
+			{
+				this->SetState(STATE_PRESSED);
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 void Button::SetState(Button::State _state)
 {
-	this->m_currentState = _state;
-	switch (_state)
+	switch (this->m_currentState =_state)
 	{
-	case Button::STATE_IDLE:
-		this->SetAnimation("IDLE");
-		break;
-	case Button::STATE_ON:
-		this->SetAnimation("ON");
-		break;
-	case Button::STATE_PRESSED:
-		this->SetAnimation("HELD");
-		break;
+		case Button::STATE_IDLE:
+			this->SetAnimation("IDLE");
+			break;
 
-	default:
-		break;
+		case Button::STATE_ON:
+			this->SetAnimation("ON");
+			break;
+
+		case Button::STATE_PRESSED:
+			this->SetAnimation("HELD");
+			break;
+
+		default:
+			break;
 	}
 }
 
@@ -129,19 +109,11 @@ Button::State Button::GetState(void)
 	return this->m_currentState;
 }
 
+void Button::Click(void)
+{
+	this->m_isClicked = true;
+}
 
 
-//ButtonText::ButtonText():Button()
-//{
-//}
-//
-//void ButtonText::FrameChanged(void)
-//{
-//	this->SpriteAnimated::FrameChanged();
-//}
-//
-//void ButtonText::draw(sf::RenderTarget& target, sf::RenderStates states) const
-//{
-//}
 
-// Button v1.0
+// Button || v1.1
