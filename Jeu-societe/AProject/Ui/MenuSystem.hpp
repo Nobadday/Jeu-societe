bce673632ae140ed114c01b0a9f8faa8879a5160
Yannou7 <yann.melisse.jf@outlook.fr>
2025-11-26 16:30:21 +0100

    fatigue

---
+++ Jeu-societe/AProject/Ui/MenuSystem.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _INC_MENUSYSTEM_HPP
+#define _INC_MENUSYSTEM_HPP
+
+
+#include "./Button.hpp"
+
+
+class MenuSystem
+{
+	private:
+
+
+	public:
+		MenuSystem(void);
+
+
+};
+
+
+
+
+
+#endif
