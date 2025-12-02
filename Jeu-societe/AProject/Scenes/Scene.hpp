d8ec604487d8effbe1935e80af8c5f78f2299507
Yannou7 <yann.melisse.jf@outlook.fr>
2025-12-02 10:36:13 +0100

    Scene (Base)  v1.2

--- Jeu-societe/AProject/Scenes/Scene.hpp
+++ Jeu-societe/AProject/Scenes/Scene.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #ifndef _INC_SCENE_HPP
 #define _INC_SCENE_HPP
 
@@ -24,19 +23,21 @@ struct SceneEvent
 	bool keepLoaded = false;
 };
 
+
 // Base virtual class to make your own scenes
 class SceneBase
 {
 	private:
 		SceneEvent m_sceneAction;
-	
+
 	protected:
 		void* m_keptData;
 
-	private:
-		void LaunchEvent(SceneEvent::Type _type);
 
 	public:
+		SceneBase(void);
+		virtual ~SceneBase(void);
+
 		SceneEvent& FetchActions(void);
 		void SetKeptData(void* _data);
 		// If keepLoaded is true, this scene should stay loaded
@@ -48,6 +49,10 @@ class SceneBase
 		virtual void PollEvent(sf::Event& _event) = 0;
 		virtual void Update(float _deltaTime) = 0;
 		virtual void Draw(sf::RenderWindow& _renderWindow) = 0;
+	
+	private:
+		void LaunchEvent(SceneEvent::Type _type);
+
 };
 
 
@@ -122,5 +127,6 @@ void SceneExample::Unload(void)
 }
 */
 
+
 #endif
-// Scene v1.1
+// Scene v1.2
