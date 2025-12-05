5b72cd2d2f5e66b8eaba3ae0b7542683b1d72c74
X33X33 <beubeu.00@proton.me>
2025-12-05 17:07:19 +0100

    je suis une fraude, nouveaux bouttons à refaire

--- Jeu-societe/AProject/main.cpp
+++ Jeu-societe/AProject/main.cpp
@@ -50,13 +50,13 @@ int main(void)
 	MainData mainData;
 
 
-	mainData.gameData.m_playerDataList.resize(2);
-	for (short i = 0; i < mainData.gameData.m_playerDataList.size(); i++)
-	{
-		mainData.gameData.m_playerDataList[i].SetJoystickID(i);
-		mainData.gameData.m_playerDataList[i].SetPlayerSkin((PlayerData::PlayerSkin)(i % 8));
-		mainData.gameData.AddPlayerPlaying(i);
-	}
+	//mainData.gameData.m_playerDataList.resize(2);
+	//for (short i = 0; i < mainData.gameData.m_playerDataList.size(); i++)
+	//{
+	//	mainData.gameData.m_playerDataList[i].SetJoystickID(i);
+	//	mainData.gameData.m_playerDataList[i].SetPlayerSkin((PlayerData::PlayerSkin)(i % 8));
+	//	mainData.gameData.AddPlayerPlaying(i);
+	//}
 
 	MainDataLoad(mainData);
 
@@ -110,8 +110,8 @@ void MainDataLoad(MainData& _mainData)
 
 	// Sélectionner le LoadingScreen metre false pour tout scene au debut 
 	//_mainData.scenes.SelectScene("Lo", false);
-	//_mainData.scenes.SelectScene("Menu", false);
-	_mainData.scenes.SelectScene("RandCard", false);
+	_mainData.scenes.SelectScene("Menu", false);
+	//_mainData.scenes.SelectScene("RandCard", false);
 	//_mainData.scenes.SelectScene("RuRoul", false);
 	//_mainData.scenes.SelectScene("Board", false);
 
