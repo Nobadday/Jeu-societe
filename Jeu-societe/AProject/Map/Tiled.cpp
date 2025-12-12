61c61c9d59e75254e24683508e4d0a8a2fa7ea48
Nobadday <nobadday8@gmail.com>
2025-12-12 13:05:28 +0100

    Update Tiled.cpp

--- Jeu-societe/AProject/Map/Tiled.cpp
+++ Jeu-societe/AProject/Map/Tiled.cpp
@@ -38,10 +38,10 @@ void Tiled::InitTiled(const char* _Map)
 				tileset.SetMargin(tilesetJson.value("margin", 0));
 				tileset.SetSpacing(tilesetJson.value("spacing", 0));
 
-				std::cout << "Chargement du tileset: " << tileset.GetName() << std::endl;
+				/*std::cout << "Chargement du tileset: " << tileset.GetName() << std::endl;
 				std::cout << "  First GID: " << tileset.GetFirstGid() << std::endl;
 				std::cout << "  Tile Count: " << tileset.GetTileCount() << std::endl;
-				std::cout << "  Tile Size: " << tileset.GetTileWidth() << "x" << tileset.GetTileHeight() << std::endl;
+				std::cout << "  Tile Size: " << tileset.GetTileWidth() << "x" << tileset.GetTileHeight() << std::endl;*/
 
 
 				// Charger les tuiles individuelles
@@ -155,10 +155,10 @@ void Tiled::InitTiled(const char* _Map)
 						const Tileset* tileset = tilesetManager.GetTilesetByGid(gid);
 						std::string tilesetName = tileset ? tileset->GetName() : "Aucun";
 
-						 std::cout << "    Objet " << j << " : " << mapObject.GetName() << " : GID=" << gid
+					/*	 std::cout << "    Objet " << j << " : " << mapObject.GetName() << " : GID=" << gid
 								   << " (Tileset: " << tilesetName << ")"
 								   << ", Pos=(" << mapObject.GetX() << "," << mapObject.GetY() << ")"
-								   << ", Taille=(" << mapObject.GetWidth() << "x" << mapObject.GetHeight() << ")" << std::endl;
+								   << ", Taille=(" << mapObject.GetWidth() << "x" << mapObject.GetHeight() << ")" << std::endl;*/
 					}
 				}
 
@@ -168,7 +168,7 @@ void Tiled::InitTiled(const char* _Map)
 			}
 		}
 
-		 std::cout << "\n=== RÉCAPITULATIF ===" << std::endl;
+		/* std::cout << "\n=== RÉCAPITULATIF ===" << std::endl;
 		 std::cout << "Nombre total de tilesets: " << tilesetManager.GetTilesetCount() << std::endl;
 		 std::cout << "Nombre total de layers: " << mapLayers.size() << std::endl;
 		   for (const auto& layer : mapLayers)
@@ -178,7 +178,7 @@ void Tiled::InitTiled(const char* _Map)
 		    std::cout << "\n=== CONTRÔLES ===" << std::endl;
 			std::cout << "Flèches directionnelles : Déplacer la caméra" << std::endl;
 			std::cout << "Échap : Quitter" << std::endl;
-			std::cout << std::endl;
+			std::cout << std::endl;*/
 	}
 }
 
